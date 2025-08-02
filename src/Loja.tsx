d98ca6848e2910e01266923df6bec881b4ccaf3e
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-08-02 12:10:46 +0200

    ( bairros na selecao  )

--- src/Loja.tsx
+++ src/Loja.tsx
@@ -1107,8 +1107,17 @@ export default function Loja() {
                   <option value="Vila Mantelli">Vila Mantelli</option>
                   <option value="Vila Real">Vila Real</option>
                   <option value="Vila Rica">Vila Rica</option>
+                  <option value="Outro">Outro...</option>
                 </select>
-
+                {/* Campo extra quando seleciona "Outro" */}
+                {address === "Outro" && (
+                  <input
+                    type="text"
+                    placeholder="Digite seu bairro"
+                    className="w-full rounded-xl border border-gray-300 bg-gray-50 px-4 py-2 text-sm text-gray-700 focus:border-red-400 focus:ring focus:ring-red-200"
+                    onChange={(e) => setAddress(e.target.value)}
+                  />
+                )}
                 <input
                   type="text"
                   placeholder="* Rua (obrigatório)"
