c1ab2bed71b9008d0bd95e26fd29a66af2d65340
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-08-21 21:12:15 +0200

    (  2 imagem no carrinho   )

--- src/Loja.tsx
+++ src/Loja.tsx
@@ -1435,9 +1435,11 @@ export default function Loja() {
                 >
                   {/* Foto do produto */}
                   <img
-                    src={item.product.imageUrl}
+                    src={
+                      item.product.imageUrl || "https://via.placeholder.com/80"
+                    }
                     alt={item.product.name}
-                    className="h-12 w-12 flex-shrink-0 rounded-md border object-contain"
+                    className="h-20 w-20 flex-shrink-0 rounded-md border object-cover"
                   />
 
                   {/* Nome + preço/quantidade */}
